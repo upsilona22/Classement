dba1cb359a71e0b9446e104bb8071446bc30d827
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-18 12:22:06 +0100

    Update downloadData.js

--- netlify/functions/downloadData.js
+++ netlify/functions/downloadData.js
@@ -1,13 +1,30 @@
-import axios from 'axios';
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-async function getUser() {
-  try {
-    const response = await axios.get('https://www.dropbox.com/s/lxvr2j4r51bi5xx/leaderboard.json?dl=0');
+ var xhr = new XMLHttpRequest();
+ xhr.open("GET", "https://www.dropbox.com/s/lxvr2j4r51bi5xx/leaderboard.json?dl=0");
 
-    return response.data;
-  } catch (err) {
-    console.log(err);
-  }
-}
+ exports.handler = async function(event, context) {
+    try {
+        const response = await dbx.filesDownload({path: "/leaderboard.json"});
 
-console.log(await getUser());
+        if (response.status !== 200) {
+            return {
+                statusCode: response.status,
+                message: "Dropbox error"
+            }
+        }
+
+        const data = JSON.parse(response.result.fileBinary);
+
+        return {
+            statusCode: 200,
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(data)
+        }
+    } catch(err) {
+        console.log(err)
+        return {
+            statusCode: 502,
+            message: "Error connecting to dropbox"
+        }
+    }
