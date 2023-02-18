fa87d644505a4fd8fc571424a6037c3a12f8399e
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-18 12:28:55 +0100

    Update downloadData.js

--- netlify/functions/downloadData.js
+++ netlify/functions/downloadData.js
@@ -1,30 +1,15 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const fetch = require('node-fetch')
 
- var xhr = new XMLHttpRequest();
- xhr.open("GET", "https://www.dropbox.com/s/lxvr2j4r51bi5xx/leaderboard.json?dl=0");
+exports.handler = async (event, context) => {
 
- exports.handler = async function(event, context) {
-    try {
-        const response = await dbx.filesDownload({path: "/leaderboard.json"});
+    const img_url = "https://www.dropbox.com/s/lxvr2j4r51bi5xx/leaderboard.json?dl=0"
 
-        if (response.status !== 200) {
-            return {
-                statusCode: response.status,
-                message: "Dropbox error"
-            }
-        }
-
-        const data = JSON.parse(response.result.fileBinary);
-
-        return {
+    return fetch(img_url)
+        .then(res => res.buffer())
+        .then((buffer) => ({
             statusCode: 200,
-            headers: { "content-type": "application/json" },
-            body: JSON.stringify(data)
-        }
-    } catch(err) {
-        console.log(err)
-        return {
-            statusCode: 502,
-            message: "Error connecting to dropbox"
-        }
-    }
+            headers: {'Content-Type': 'image/jpeg'},
+            isBase64Encoded: true,
+            body: buffer.toString('base64'),
+        }))
+}
