4d5307d7aaab8fc38267817fb8fb5d5740dd8991
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-18 12:16:46 +0100

    Update downloadData.js

--- netlify/functions/downloadData.js
+++ netlify/functions/downloadData.js
@@ -1,15 +1,13 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
- 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://www.dropbox.com/s/lxvr2j4r51bi5xx/leaderboard.json?dl=0");
- 
-xhr.onreadystatechange = function () {
-    console.log("readyState = " + this.readyState + ", status = " + this.status);
-    if (this.readyState == 4 && this.status == 200) {
-        var result = this.responseText;
-        console.log(result);
-    }
-};
- 
-xhr.send();
+import axios from 'axios';
 
+async function getUser() {
+  try {
+    const response = await axios.get('https://www.dropbox.com/s/lxvr2j4r51bi5xx/leaderboard.json?dl=0');
+
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+console.log(await getUser());
