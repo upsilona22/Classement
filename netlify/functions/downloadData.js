87047afd227e5d30195e7bba4076c70f1250c55b
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-18 12:33:00 +0100

    Update downloadData.js

--- netlify/functions/downloadData.js
+++ netlify/functions/downloadData.js
@@ -1,15 +1,24 @@
-const fetch = require('node-fetch')
+const fileType = require('file-type');
 
-exports.handler = async (event, context) => {
+exports.handler = async () => {
+ 
+  // Use a PDF
+  const file = 'hhttps://www.dropbox.com/s/lxvr2j4r51bi5xx/leaderboard.json?dl=0'
+  // Or even an iamge
+  //const file = 'https://example.com/sample.jpg'
+  
+  const response = await fetch(file);
+  const status_code = response.status
+  const content_type = response.headers.get('content-type')
+  const buffer = await response.buffer();
+  const type = await fileType.fromBuffer(buffer)
 
-    const img_url = "https://www.dropbox.com/s/lxvr2j4r51bi5xx/leaderboard.json?dl=0"
-
-    return fetch(img_url)
-        .then(res => res.buffer())
-        .then((buffer) => ({
-            statusCode: 200,
-            headers: {'Content-Type': 'image/jpeg'},
-            isBase64Encoded: true,
-            body: buffer.toString('base64'),
-        }))
+  return {
+    statusCode: status_code,
+    headers: {
+      'Content-Type': content_type,
+    },
+    isBase64Encoded: true,
+    body: buffer.toString('base64')
+  }
 }
