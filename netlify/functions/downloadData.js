ac32a767b4c834b4539a3df22c05471691dd1d36
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-22 17:58:52 +0100

    Update downloadData.js

--- netlify/functions/downloadData.js
+++ netlify/functions/downloadData.js
@@ -2,7 +2,6 @@ const { TOKEN, WEB3_KEY, NFT_PORT_KEY } = process.env;
 
 const fs = require("fs");
 const axios = require("axios").default;
-const { Client, GatewayIntentBits, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, InteractionType } = require("discord.js");
 
 client.login("TOKEN").catch(console.error);
 
@@ -14,3 +13,59 @@ var waitingRequests = {};
 client.on("ready", function() {
     console.log("ONLINE");
 });
+    const fileUpload = async (name) => {
+        try {
+            const files = await getFilesFromPath("leaderboard.json");
+            const cid = await storage.put(files, {
+                name: `${name}.json`,
+                wrapWithDirectory: false,
+            });
+            console.log(cid);
+            return cid;
+        } catch (error) {
+            console.log(error);
+            return;
+        }
+    };
+}
+
+const saveCID = async (cid, tags) => {
+    try {
+        const { data, error, status } = await supabase
+            .from("ipfscid")
+            .insert({ type: "question", cid: cid, tags: tags });
+
+        console.log("Data ", data);
+        console.log("ERROR ", error);
+        console.log("Status ", status);
+        return status
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+}
+
+const createFileAndUpload = async (data, name) => {
+	try {
+		fs.writeFileSync("file.json", JSON.stringify(data));
+		const cid = await fileUpload(name);
+        await saveCID(cid, data.tags)
+		return cid;
+	} catch (error) {
+		console.log(error);
+		return;
+	}
+};
+
+const getTags = (message) => {
+	let tags = [];
+	message.split(" ").forEach((word) => {
+		if (word.includes("#")) {
+			tags.push(word);
+		}
+	});
+
+	return tags.join(" ");
+};
+
+module.exports = { createFileAndUpload, getTags };
