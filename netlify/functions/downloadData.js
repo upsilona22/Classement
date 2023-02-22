d009fea5ec988614a153cbc88d3c32c07b087089
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-22 18:04:31 +0100

    Update downloadData.js

--- netlify/functions/downloadData.js
+++ netlify/functions/downloadData.js
@@ -3,7 +3,7 @@ const { TOKEN, WEB3_KEY, NFT_PORT_KEY } = process.env;
 const fs = require("fs");
 const axios = require("axios").default;
 
-client.login("TOKEN").catch(console.error);
+const token = process.TOKEN;
 
 const { Web3Storage, File, Blob } = require("web3.storage");
 const Web3Client = new Web3Storage({ token: WEB3_KEY });
