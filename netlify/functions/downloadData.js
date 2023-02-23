6cec5d9ae2959468efb234063ebd85a5709ea38f
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-23 11:20:46 +0100

    Update downloadData.js

--- netlify/functions/downloadData.js
+++ netlify/functions/downloadData.js
@@ -1,22 +1,7 @@
-const fetch = require('node-fetch');
-require('dotenv').config();
-
-
-exports.handler = async (event, context) => {
-  try {
-    const response = await fetch('https://www.dropbox.com/s/lxvr2j4r51bi5xx/leaderboard.json?dl=0');
-    const data = await response.json();
-    const json = JSON.stringify({ data });
-    
-    return { 
-      statusCode: 200, 
-      body: json
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed fetching data' }),
-    };
-  }
-};
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    'content-type': 'application/json',
+    body: JSON.stringify({message: "Hello World"})
+  };
+}
