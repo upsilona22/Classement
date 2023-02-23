e7dd5f77a7ca25763c2ee2ee3a5999858c7c59e0
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-23 11:22:37 +0100

    Update downloadData.js

--- netlify/functions/downloadData.js
+++ netlify/functions/downloadData.js
@@ -2,6 +2,45 @@ exports.handler = async function(event, context) {
   return {
     statusCode: 200,
     'content-type': 'application/json',
-    body: JSON.stringify({message: "Hello World"})
+    body: JSON.stringify({
+    "1": {
+        "Name": "JOJO",
+        "Demolitions": 3685,
+        "Exterminations": 16,
+        "LastUpdate": "2023-02-09T00:00:00.000",
+        "Authorized": 0,
+        "History": [
+            {
+                "Demolitions": 3685,
+                "Exterminations": 16,
+                "Time": "2023-02-09T00:00:00.000"
+            }
+        ]
+    },
+    "775654181167562752": {
+        "Name": "upsilon",
+        "Demolitions": 10100,
+        "Exterminations": 102,
+        "LastUpdate": "2023-02-23T09:52:19.900Z",
+        "Authorized": 0,
+        "History": [
+            {
+                "Demolitions": 12,
+                "Exterminations": 1,
+                "Time": "2023-02-23T09:44:50.650Z"
+            },
+            {
+                "Demolitions": 10004,
+                "Exterminations": 98,
+                "Time": "2023-02-23T09:52:04.786Z"
+            },
+            {
+                "Demolitions": 10100,
+                "Exterminations": 102,
+                "Time": "2023-02-23T09:52:19.900Z"
+            }
+        ]
+    }
+})
   };
 }
