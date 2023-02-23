7719cbc8bf78473ff4445e887660c89a48467edc
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-23 11:14:17 +0100

    Update downloadData.js

--- netlify/functions/downloadData.js
+++ netlify/functions/downloadData.js
@@ -1,40 +1,22 @@
-{
-    "1": {
-        "Name": "JOJO",
-        "Demolitions": 3685,
-        "Exterminations": 16,
-        "LastUpdate": "2023-02-09T00:00:00.000",
-        "Authorized": 0,
-        "History": [
-            {
-                "Demolitions": 3685,
-                "Exterminations": 16,
-                "Time": "2023-02-09T00:00:00.000"
-            }
-        ]
-    },
-    "775654181167562752": {
-        "Name": "upsilon",
-        "Demolitions": 10100,
-        "Exterminations": 102,
-        "LastUpdate": "2023-02-23T09:52:19.900Z",
-        "Authorized": 0,
-        "History": [
-            {
-                "Demolitions": 12,
-                "Exterminations": 1,
-                "Time": "2023-02-23T09:44:50.650Z"
-            },
-            {
-                "Demolitions": 10004,
-                "Exterminations": 98,
-                "Time": "2023-02-23T09:52:04.786Z"
-            },
-            {
-                "Demolitions": 10100,
-                "Exterminations": 102,
-                "Time": "2023-02-23T09:52:19.900Z"
-            }
-        ]
-    }
-}
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+
+exports.handler = async (event, context) => {
+  try {
+    const response = await fetch('https://www.dropbox.com/s/lxvr2j4r51bi5xx/leaderboard.json?dl=0');
+    const data = await response.json();
+    const json = JSON.stringify({ data });
+    
+    return { 
+      statusCode: 200, 
+      body: json
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' }),
+    };
+  }
+};
