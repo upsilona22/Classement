c48293fbc56121ab1a30fe22f68915b1f30f9ca7
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-24 10:33:36 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,39 +1,10 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import styles from './footer.module.css';
+import styles from './Footer.module.css'
 
-export function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <h3 className={styles.sectionHeading}>FRENCH DEMO COMMUNITY</h3>
-      <nav>
-        <ul className={styles.nav}>
-          <li>
-            <strong className={styles.heading}>Most important link</strong>
-            <Link className={styles.link} to="/pages/shipping-and-return-policy">
-              Shipping & Returns policy
-            </Link>
-          </li>
-          <li>
-            <strong className={styles.heading}>Contact us</strong>
-            <a className={styles.link} href="mailto:contact@swag.netlify.com">
-              contact@swag.netlify.com
-            </a>
-          </li>
-          <li>
-            <strong className={styles.heading}>Executive Producer</strong>
-            <a className={styles.link} href="https://www.netlify.com/">
-              Netlify
-            </a>
-          </li>
-          <li>
-            <strong className={styles.heading}>Starring</strong>
-            <a className={styles.link} href="https://github.com/netlify/swag-site/graphs/contributors">
-              Contributors
-            </a>
-          </li>
-        </ul>  
-      </nav>
-    </footer>
-  );
-}
+ export default function Footer() {
+   return (
+     <>
+       <footer className={styles.footer}>
+         Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+       </footer>
+     </>
+   )
