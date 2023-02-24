87d916e033a173887f76c85d871a336a7c976e9b
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-24 10:26:16 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,25 +1,39 @@
-import styles from './Footer.module.css'
+import React from 'react';
+import { Link } from 'gatsby';
+import styles from './footer.module.css';
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <div
-        style={{
-          backgroundColor: "wheat",
-          marginTop: "5rem",
-          minHeight: "5rem",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        French Demo Community
-        <img
-          alt="img"
-          style={{ width: "20px", height: "20px" }}
-          src="https://e7.pngegg.com/pngimages/810/372/png-clipart-blue-and-white-sports-car-logo-rocket-league-logo-t-shirt-rocket-league-blue-text-thumbnail.png"
-        />
-      </div>
-    );
-  }
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <h3 className={styles.sectionHeading}>FRENCH DEMO COMMUNITY</h3>
+      <nav>
+        <ul className={styles.nav}>
+          <li>
+            <strong className={styles.heading}>Most important link</strong>
+            <Link className={styles.link} to="/pages/shipping-and-return-policy">
+              Shipping & Returns policy
+            </Link>
+          </li>
+          <li>
+            <strong className={styles.heading}>Contact us</strong>
+            <a className={styles.link} href="mailto:contact@swag.netlify.com">
+              contact@swag.netlify.com
+            </a>
+          </li>
+          <li>
+            <strong className={styles.heading}>Executive Producer</strong>
+            <a className={styles.link} href="https://www.netlify.com/">
+              Netlify
+            </a>
+          </li>
+          <li>
+            <strong className={styles.heading}>Starring</strong>
+            <a className={styles.link} href="https://github.com/netlify/swag-site/graphs/contributors">
+              Contributors
+            </a>
+          </li>
+        </ul>  
+      </nav>
+    </footer>
+  );
 }
