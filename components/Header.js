306a3eaa878ee67ed4227622728e769c5275a6ee
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-24 10:49:58 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,3 @@
 export default function Header({ title }) {
    return <h1 className="title">{title}</h1>
  }
-
-$(function(){
-  $(window).scroll(function(){
-    var winTop = $(window).scrollTop();
-    if(winTop >= 30){
-      $("body").addClass("sticky-header");
-    }else{
-      $("body").removeClass("sticky-header");
-    }//if-else
-  });//win func.
-});//ready func.
