183709f660bd59231094f69d03c2e638d6aec22b
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-24 10:16:03 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,27 @@
 import styles from './Footer.module.css'
 
-export default function Footer() {
-  return (
-    <>
-      <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
-      </footer>
-    </>
-  )
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          backgroundColor: "wheat",
+          marginTop: "5rem",
+          minHeight: "5rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        French Demo Community
+        <img
+          alt="img"
+          style={{ width: "20px", height: "20px" }}
+          src="https://e7.pngegg.com/pngimages/810/372/png-clipart-blue-and-white-sports-car-logo-rocket-league-logo-t-shirt-rocket-league-blue-text-thumbnail.png"
+        />
+      </div>
+    );
+  }
 }
