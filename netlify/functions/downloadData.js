ee9a88f4e32f5ea9fc249b90106e4238a5e447f2
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-25 13:38:00 +0100

    Update downloadData.js

--- netlify/functions/downloadData.js
+++ netlify/functions/downloadData.js
@@ -5,15 +5,29 @@ exports.handler = async function(event, context) {
     body: JSON.stringify({
     "1": {
         "Name": "BOT",
-        "Demolitions": 1,
-        "Exterminations": 0,
+        "Demolitions": 10,
+        "Exterminations": 1,
         "LastUpdate": "2023-02-09T00:00:00.000",
         "Authorized": 0,
         "History": [
             {
-                "Demolitions": 1,
-                "Exterminations": 0,
+                "Demolitions": 10,
+                "Exterminations": 1,
                 "Time": "2023-02-09T00:00:00.000"
+            }
+        ]
+    },
+    "325920058146816000": {
+        "Name": "FuriousPanda",
+        "Demolitions": 138017,
+        "Exterminations": 10697,
+        "LastUpdate": "2023-02-25T12:33:33.028Z",
+        "Authorized": 0,
+        "History": [
+            {
+                "Demolitions": 14000,
+                "Exterminations": 1,
+                "Time": "2023-02-25T12:33:33.028Z"
             }
         ]
     }
