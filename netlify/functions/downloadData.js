bfe65ed645f3666f78eada14316e554e66cb66dc
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-25 11:26:16 +0100

    Update downloadData.js

--- netlify/functions/downloadData.js
+++ netlify/functions/downloadData.js
@@ -4,30 +4,16 @@ exports.handler = async function(event, context) {
     'content-type': 'application/json',
     body: JSON.stringify({
     "1": {
-        "Name": "JOJO",
-        "Demolitions": 10,
-        "Exterminations": 1,
+        "Name": "BOT",
+        "Demolitions": 1,
+        "Exterminations": 0,
         "LastUpdate": "2023-02-09T00:00:00.000",
         "Authorized": 0,
         "History": [
             {
-                "Demolitions": 10,
-                "Exterminations": 1,
+                "Demolitions": 1,
+                "Exterminations": 0,
                 "Time": "2023-02-09T00:00:00.000"
-            }
-        ]
-    },
-    "708235443875086379": {
-        "Name": "blanco2wishhh",
-        "Demolitions": 15,
-        "Exterminations": 1,
-        "LastUpdate": "2023-02-23T14:08:25.643Z",
-        "Authorized": 0,
-        "History": [
-            {
-                "Demolitions": 15,
-                "Exterminations": 1,
-                "Time": "2023-02-23T14:08:25.643Z"
             }
         ]
     }
