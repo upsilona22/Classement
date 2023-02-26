6c3b1c9115b37989dd1e9098ac1889964df24fdf
upsilona22 <123645115+upsilona22@users.noreply.github.com>
2023-02-26 13:43:39 +0100

    Update downloadData.js

--- netlify/functions/downloadData.js
+++ netlify/functions/downloadData.js
@@ -30,7 +30,85 @@ exports.handler = async function(event, context) {
                 "Time": "2023-02-25T12:33:33.028Z"
             }
         ],
-        "Country": "FRA"
+        "Country": "FRANCE"
+    },
+    "775654181167562752": {
+        "Name": "Upsilon",
+        "Demolitions": 3690,
+        "Exterminations": 1,
+        "LastUpdate": "2023-02-26T12:39:43.318Z",
+        "Authorized": 0,
+        "History": [
+            {
+                "Demolitions": 3685,
+                "Exterminations": 1,
+                "Time": "2023-02-25T12:44:45.536Z"
+            },
+            {
+                "Demolitions": 3686,
+                "Exterminations": 1,
+                "Time": "2023-02-25T15:13:33.325Z"
+            },
+            {
+                "Demolitions": 3690,
+                "Exterminations": 1,
+                "Time": "2023-02-26T12:39:43.318Z"
+            }
+        ],
+        "Country": "France"
+    },
+    "318505056926171136": {
+        "Name": "susu",
+        "Demolitions": 899,
+        "Exterminations": 1,
+        "LastUpdate": "2023-02-25T13:00:50.346Z",
+        "Authorized": 0,
+        "History": [
+            {
+                "Demolitions": 899,
+                "Exterminations": 1,
+                "Time": "2023-02-25T13:00:50.346Z"
+            }
+        ]
+    },
+    "1034806809078280213": {
+        "Name": "Velvet-crowe33",
+        "Demolitions": 15646,
+        "Exterminations": 765,
+        "LastUpdate": "2015-07-07T00:00:00.000",
+        "Authorized": 0,
+        "History": [],
+        "Country": "France"
+    },
+    "836252471487496292": {
+        "Name": "bxee.",
+        "Demolitions": 13322,
+        "Exterminations": 242,
+        "LastUpdate": "2023-02-25T20:08:15.419Z",
+        "Authorized": 0,
+        "History": [
+            {
+                "Demolitions": 13322,
+                "Exterminations": 242,
+                "Time": "2023-02-25T20:08:15.419Z"
+            }
+        ],
+        "Country": "France"
+    },
+    "705859393274511391": {
+        "Name": "Guillaume_LP",
+        "Demolitions": 11410,
+        "Exterminations": 6,
+        "LastUpdate": "2023-02-26T07:25:20.094Z",
+        "Authorized": 0,
+        "History": [
+            {
+                "Demolitions": 11410,
+                "Exterminations": 6,
+                "Time": "2023-02-26T07:25:20.094Z"
+            }
+        ],
+        "Country": "Bretagne"
     }
 })
   };
